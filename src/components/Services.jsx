35b302fdff084009dc2484a71f724639f7768695
dhanujha <dhanujha@gogosoon.com>
2021-04-09 17:04:07 +0530

    [Dhanujha] Increases opacity of services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,51 +2,39 @@ import React from "react";
 
 function Services() {
   return (
-    <div>
-      <section id="services">
-        <div className="overlay"></div>
+    <section id="services">
+      <div className="overlay"></div>
 
-        <div className="row section-intro">
-          <div className="col-twelve">
-            <h5>Services</h5>
-            <h1>What Can I Do For You?</h1>
-          </div>
+      <div className="row section-intro">
+        <div className="col-twelve">
+          <h5>Services</h5>
+          <h1>What Can I Do For You?</h1>
         </div>
-        <div className="row services-content">
-          <div className="services-list">
-          <div className="service">
+      </div>
+      <div className="row section-intro">
+        <div className="services-list">
+          <div className="col-six">
             <span className="icon">
               <i className="icon-earth"></i>
             </span>
 
             <div className="service-content">
-              <h3>Webdesign</h3>
+              <h3>Websites</h3>
             </div>
           </div>
 
-          <div className="service">
+          <div className="col-six">
             <span className="icon">
               <i className="icon-window"></i>
             </span>
 
             <div className="service-content">
-              <h3>Web Development</h3>
-            </div>
-          </div>
-
-          <div className="service">
-            <span className="icon">
-              <i className="icon-toggles"></i>
-            </span>
-
-            <div className="service-content">
-              <h3>UI/UX Design</h3>
+              <h3>Mobile Apps</h3>
             </div>
           </div>
         </div>
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
 
