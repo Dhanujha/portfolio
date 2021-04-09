e43938ff4388283d43efd0926e6f5a9f58913d06
dhanujha <dhanujha@gogosoon.com>
2021-04-09 20:44:37 +0530

    revert to original code

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-export default async function contactHandler(req, res) {
+export default function contactHandler(req, res) {
   const { name, email, subject, message } = req.body;
   var data = JSON.stringify({
     chat_id: process.env.TELEGRAM_USER_NAME_ID,
@@ -15,19 +15,12 @@ export default async function contactHandler(req, res) {
     },
     data: data,
   };
-  try {
-    const d = await axios(config);
-    await res.status(200).json({
-      status: "success",
-      message: "Your message was sent, thank you!",
-      other: d,
-    });
-  } catch (e) {
-    console.log(e);
-    await res.status(400).json({
-      status: "error",
-      message: "Something went wrong, please try again",
-      other: e,
-    });
-  }
+
+  axios(config).catch((err) => {
+    console.log(err.message);
+  });
+  res.status(200).json({
+    status: "success",
+    message: "Your message was sent, thank you!",
+  });
 }
